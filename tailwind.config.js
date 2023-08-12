e7de5ab0b885ab32568ffb5dd5221757ded8a971
Debaditya Bhar <bhar.debaditya@gmail.com>
2023-08-12 13:10:47 +0530

    Version 1.0.0 - Initial setup with styles, fonts, favicons and color pallate.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                karla: ["Karla", "sans-serif"],
+                sans: ["Open Sans", "sans-serif"],
+                sacramento: ["Sacramento", "sans-serif"],
+                ubuntu: ["Ubuntu", "sans-serif"],
+            },
+            colors: {
+                "pallete-color1": "#F72585",
+                "pallete-color2": "#7209B7",
+                "pallete-color3": "#480CA8",
+                "pallete-color4": "#3F37C9",
+                "pallete-color5": "#4CC9F0",
+                "light-color": "#F5F5F5",
+                "dark-color": "#3C1642",
+                "facebook-color": "#3b5998",
+                "insta-color": "#d6249f",
+                "github-color": "#f4a261",
+                "twitter-color": "#1da1f2",
+                "linkedin-color": "#0E76A8",
+            },
+            screens: {
+                sm: { min: "280px", max: "530px" },
+                // => @media (min-width: 280px and max-width: 530px) { ... }
+
+                md: { min: "540px", max: "1024px" },
+                // => @media (min-width: 540px and max-width: 1024px) { ... }
+
+                lg: { min: "1025px" },
+                // => @media (min-width: 1025px) { ... }
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
