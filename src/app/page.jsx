d75ea902937c41ffd382986ca23d75c57672d73b
Debaditya Bhar <bhar.debaditya@gmail.com>
2023-08-13 17:39:49 +0530

    Version 1.0.8 - Created home page.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,7 @@ export default function Home() {
         <main
             className="w-full min-h-screen bg-light-color px-20 sm:px-10 py-10 sm:py-5 
             flex flex-col items-start justify-center">
-            <section className="w-full flex flex-row sm:flex-col justify-between items-center space-x-8">
+            <section className="w-full flex flex-row sm:flex-col justify-between items-center space-x-8 sm:space-y-8">
                 <Image
                     className="block w-[40%] md:w-1/2 sm:w-full rounded-3xl shadow-lg shadow-gray-400 animate-zoom-in"
                     src={HeroImg1}
