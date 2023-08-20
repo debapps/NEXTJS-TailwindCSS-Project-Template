ededa56073788b6138db6af21815ce5e383f5726
Debaditya Bhar <bhar.debaditya@gmail.com>
2023-08-20 21:36:01 +0530

    Version 1.2.7 - Completed Home page.

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 This is NEXT JS project starter template designed with Tailwind CSS.
 
+Implemetating the complete front-end features for your NEXT JS + Tailwind CSS project.
+
+-   Responsive web designing.
+-   React WYSIWYG Rich Text Editor. (using JoditEditor)
+-   OAuth Integration with NEXT JS. (using AUTH.JS)
+-   Remote API calls. (using fetch api and useSWR hooks)
+-   Easy-to-use Annimations. (using simple CSS annimation and Freamer Motion)
+-   Implemetating Dark mode. (using custom hooks)
+-   Other Ready-made Front-end tools.
+
 ## Environment Variable
 
 -   **NEXT_PUBLIC_API_URL:** This is the hosting URL for backend API (**Story-API**: "https://story-api-45u7.onrender.com").
@@ -9,3 +19,7 @@ This is NEXT JS project starter template designed with Tailwind CSS.
 -   **GOOGLE_CLIENT_SECRET:** This is Google OAuth client secret.
 -   **NEXTAUTH_SECRET:** This is Next Auth Secret (Any random string).
 -   **NEXTAUTH_URL:** This is Next Auth URL (This is Application hosting URL)
+
+## Production URL
+
+[Hosted on Vercel](https://next-tailwind-ebon.vercel.app)
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import AnimatedHeading from "./components/AnimatedHeading";
 import HeroImg1 from "../../public/images/web-dev-hero.jpg";
+import HeroImg2 from "../../public/images/api-nextjs.jpg";
+import HeroImg3 from "../../public/images/programming-tools.jpg";
+import GradientText from "./components/GradientText";
 
 export const metadata = {
     title: "Home | NEXT JS + Tailwind CSS",
@@ -13,9 +16,10 @@ export default function Home() {
         <main
             className="w-full min-h-screen bg-light-color dark:bg-dark-color 
             px-20 sm:px-10 py-40 sm:py-5 flex flex-col items-center justify-center overflow-x-hidden">
+            {/* Section 1 */}
             <section
                 className="w-full flex flex-row md:flex-col sm:flex-col justify-between items-center 
-                space-x-8 md:space-x-0 sm:space-x-0 md:space-y-16 sm:space-y-8">
+                space-x-8 md:space-x-0 sm:space-x-0 md:space-y-16 sm:space-y-8 py-24 md:py-10 sm:py-5">
                 <Image
                     className="block w-[40%] md:w-full sm:w-full rounded-3xl 
                     shadow-2xl shadow-pallete-color3 dark:shadow-pallete-color5 
@@ -41,6 +45,86 @@ export default function Home() {
                         Fill free to use the template in your next front end
                         project.
                     </p>
+                </article>
+            </section>
+            <hr
+                className="w-full h-0 border-t border-dashed border-dark-color 
+              dark:border-light-color border-separate border-spacing-1"
+            />
+            {/* Section 2 */}
+            <section
+                className="w-full flex flex-row md:flex-col sm:flex-col justify-between items-center 
+                space-x-8 md:space-x-0 sm:space-x-0 md:space-y-16 sm:space-y-8 py-24 md:py-10 sm:py-5">
+                <article className="flex flex-col items-start justify-center w-[60%] md:w-full sm:w-full">
+                    <GradientText
+                        sx="text-6xl md:text-4xl sm:text-2xl font-bold font-ubuntu
+                        leading-tight mb-10">
+                        Integrating REST API in NEXT JS 13 and Annimation.
+                    </GradientText>
+                    <p className="animate-text-in">
+                        Implemetating the remote REST API calls using the{" "}
+                        <strong>NEXT JS 13</strong> frameworks.
+                    </p>
+                    <ul className="animate-text-in">
+                        <li>
+                            JavaScript <strong>fetch</strong> API call.
+                        </li>
+                        <li>
+                            React specific <strong>useSWR</strong> hooks.
+                        </li>
+                    </ul>
+                    <p className="animate-text-in">
+                        Implemetating annimations using simple Tailwind CSS
+                        framework and <strong>Framer Motion</strong> Library.
+                    </p>
+                </article>
+                <Image
+                    className="block w-[40%] md:w-full sm:w-full rounded-3xl 
+                    shadow-2xl shadow-pallete-color3 dark:shadow-pallete-color5 
+                    animate-zoom-in sm:mt-52 sm:mb-5"
+                    src={HeroImg2}
+                    alt="hero-image-2"
+                    priority
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
+                />
+            </section>
+            <hr
+                className="w-full h-0 border-t border-dashed border-dark-color 
+              dark:border-light-color border-separate border-spacing-1"
+            />
+            {/* Section 3 */}
+            <section
+                className="w-full flex flex-row md:flex-col sm:flex-col justify-between items-center 
+                space-x-8 md:space-x-0 sm:space-x-0 md:space-y-16 sm:space-y-8 py-24 md:py-10 sm:py-5">
+                <Image
+                    className="block w-[40%] md:w-full sm:w-full rounded-3xl 
+                    shadow-2xl shadow-pallete-color3 dark:shadow-pallete-color5 
+                    animate-zoom-in sm:mt-52 sm:mb-5"
+                    src={HeroImg3}
+                    alt="hero-image-3"
+                    priority
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
+                />
+                <article className="flex flex-col items-start justify-center w-[60%] md:w-full sm:w-full">
+                    <GradientText
+                        sx="text-6xl md:text-4xl sm:text-2xl font-bold font-ubuntu
+                        leading-tight mb-10">
+                        Your Complete Front-End Programming Toolbox.
+                    </GradientText>
+
+                    <p className="animate-text-in">
+                        Implemetating the complete front-end features for your
+                        NEXT JS + Tailwind CSS project.
+                    </p>
+                    <ul className="animate-text-in">
+                        <li>Responsive web designing.</li>
+                        <li>React WYSIWYG Rich Text Editor.</li>
+                        <li>OAuth Integration with NEXT JS</li>
+                        <li>Remote API calls.</li>
+                        <li>Easy-to-use Annimations.</li>
+                        <li>Implemetating Dark mode.</li>
+                        <li>Other Ready-made Front-end tools.</li>
+                    </ul>
                 </article>
             </section>
         </main>
